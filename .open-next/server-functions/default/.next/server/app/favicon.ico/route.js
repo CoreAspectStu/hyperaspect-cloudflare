b3f0b51da96037e78845f44b682eae456939892b
Stuart <stuart@coreaspect.com>
2026-06-18 19:13:24 +1000

    chore: gitignore .open-next and .wrangler build artifacts

--- .open-next/server-functions/default/.next/server/app/favicon.ico/route.js
+++
@@ -1,7 +0,0 @@
-var R=require("../../chunks/[turbopack]_runtime.js")("server/app/favicon.ico/route.js")
-R.c("server/chunks/[externals]_next_dist_0iuj5m_._.js")
-R.c("server/chunks/[root-of-the-server]__1njnywk._.js")
-R.c("server/chunks/0k5d_next_dist_esm_build_templates_app-route_1zierge.js")
-R.c("server/chunks/_next-internal_server_app_favicon_ico_route_actions_0g2jjls.js")
-R.m(36323)
-module.exports=R.m(36323).exports
--- .open-next/server-functions/default/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"wF8sIK2foSDJX1lZlWrP4MC8zlteQKhEVE4487G3hSE=\"\n}"
