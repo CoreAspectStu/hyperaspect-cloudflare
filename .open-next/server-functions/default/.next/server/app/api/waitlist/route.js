436113b404fa162bdd7c7c14ed4d836647f99277
Stuart <stuart@coreaspect.com>
2026-06-18 19:13:03 +1000

    feat: Cloudflare Workers deployment via @opennextjs/cloudflare — LIVE at hyperaspect.stuart-61c.workers.dev

---
+++ .open-next/server-functions/default/.next/server/app/api/waitlist/route.js
@@ -0,0 +1,6 @@
+var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/waitlist/route.js")
+R.c("server/chunks/[root-of-the-server]__00t8g-z._.js")
+R.c("server/chunks/[root-of-the-server]__1njnywk._.js")
+R.c("server/chunks/_next-internal_server_app_api_waitlist_route_actions_1f_7jl_.js")
+R.m(42974)
+module.exports=R.m(42974).exports
---
+++ .open-next/server-functions/default/.next/server/middleware-build-manifest.js
@@ -0,0 +1,20 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
+  },
+  "devFiles": [],
+  "polyfillFiles": [
+    "static/chunks/0cz1d0mv5g_q7.js"
+  ],
+  "lowPriorityFiles": [
+    "static/22kILCPDYdntTFu3ljWUO/_buildManifest.js",
+    "static/22kILCPDYdntTFu3ljWUO/_ssgManifest.js",
+    "static/22kILCPDYdntTFu3ljWUO/_clientMiddlewareManifest.js"
+  ],
+  "rootMainFiles": [
+    "static/chunks/3qyl_k7rzis27.js",
+    "static/chunks/409_h2xnvid7j.js",
+    "static/chunks/0hha91vaoysnq.js",
+    "static/chunks/turbopack-0o29-mo1fdnaq.js"
+  ]
+};
